0989904dc7852dc6b770f042ab4bb56d3a88943c
LuK1337 <priv.luk@gmail.com>
2018-07-06 14:53:18 +0200

    sm6125-common: Override AVB version in kernel cmdline
    * Fixes vintf vendor <> system mismatches, see:
      Runtime info <==> Framework Compatibility Matrix? 0, AVB version 1.0 does not match framework matrix 0.0
    
    Change-Id: Ia26a84cb38e2f03025809a3ec52587481a6d8217

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += androidboot.avb_version=1.0 androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
