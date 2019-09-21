5d229a120ed3836b1b989a7d5f9f59533bc3a04d
Peter Cai <peter@typeblog.net>
2019-09-21 16:55:22 +0800

    sm6125-common: drop prebuilt libstagefright-omx
    * This is in vndk, not in vendor.
    
    Change-Id: I7334f7540662f4e8198b92ec75823b448441ccb6

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -290,6 +290,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += \
+    libstagefright_omx.vendor \
+    android.hardware.media.omx@1.0-service
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
