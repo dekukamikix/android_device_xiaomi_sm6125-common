2f0a66d024299a24d2d01a278ba4080fa949236d
Ashwin R C <ashwin2001achu@gmail.com>
2019-12-23 08:41:15 +0000

    Revert "ginkgo: build omx blobs from source"
    This reverts commit 276c9fdc393ae748460371ede71b3bf8d1b94d66.

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -298,21 +298,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
-# OMX
-PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
-    libOmxVdec \
-    libOmxVenc \
-    libc2dcolorconvert \
-    libmm-omxcore \
-    libstagefrighthw \
-    libplatformconfig
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.xiaomi_sm6125
