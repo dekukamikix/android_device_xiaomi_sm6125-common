fb218f9549501b2f1135b597f7c9f531165153e5
Ashwin R C <ashwin2001achu@gmail.com>
2019-12-25 11:34:03 +0000

    sm6125-common: build camera hidl from source

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -125,6 +125,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     libxml2 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     Snap
 
 # CNE
